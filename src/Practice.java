f0a4316d948d521ca1fa2d329a50399ec6cb7eeb
alstondsouza1 <alstondsouza35@gmail.com>
2025-01-07 14:37:40 -0800

    solved the countFrequencies with Time and Space Complexity

--- src/Practice.java
+++ src/Practice.java
@@ -28,8 +28,8 @@ public class Practice {
   }
   
 
-  // Time Complexity: 
-  // Space Complexity: 
+  // Time Complexity: 0(n)
+  // Space Complexity: 0(n)
   // Does the 'T' look confusing? Consider refreshing on generic methods
   // We'll revisit generics as a class later
   public static <T> Map<T, Integer> countFrequencies(T[] array) {
