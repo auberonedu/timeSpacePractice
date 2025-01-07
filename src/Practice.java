e5d039452c01e55cce9750f87e93c4e7bc6b3ae8
Stefanurkal <stephenfranada@gmail.com>
2025-01-07 14:39:16 -0800

    first 2 completed

--- src/Practice.java
+++ src/Practice.java
@@ -5,8 +5,8 @@ import java.util.Map;
 
 public class Practice {
 
-  // Time Complexity:
-  // Space Complexity: 
+  // Time Complexity:n(n^2)
+  // Space Complexity: O(1)
   public static List<Integer> findEvens(int[] array) {
     List<Integer> evens = new ArrayList<>();
     for (int num : array) {
@@ -17,8 +17,8 @@ public class Practice {
     return evens;
   }
 
-  // Time Complexity:
-  // Space Complexity: 
+  // Time Complexity: O(n^2)
+  // Space Complexity: O(1)
   public static int sumDiagonal(int[][] matrix) {
     int sum = 0;
     for (int i = 0; i < matrix.length; i++) {
