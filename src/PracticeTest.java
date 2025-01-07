7664eb346e0841d8bcc4d23179c8664fa639c2bc
Augy Markham <katiefaithmarkham@gmail.com>
2025-01-07 15:09:04 -0800

    completed testMostCommonSpaceEfficient - finished assignment

--- src/PracticeTest.java
+++ src/PracticeTest.java
@@ -4,13 +4,20 @@ import static org.junit.jupiter.api.Assertions.*;
 
 public class PracticeTest {
   
-  // TODO: Implement tests for Practice.mostCommonTimeEfficient and Practice.mostCommonSpaceEfficient
+  // Implement tests for Practice.mostCommonTimeEfficient and Practice.mostCommonSpaceEfficient
 
   // Hints: They are static methods, so you will use the full Practice.mostCommonTimeEfficient for method calls
   // Forgot how to do unit tests? Look back at the ramblebot tests or old slides for a refresher!
 
   @Test
   void testMostCommonTimeEfficient() {
+    int[] test = {1, 2, 2, 3, 4};
+
+    assertEquals(2, Practice.mostCommonTimeEfficient(test));
+  }
+
+  @Test
+  void testMostCommonSpaceEfficient() {
     int[] test = {1, 2, 2, 3, 4};
 
     assertEquals(2, Practice.mostCommonSpaceEfficient(test));
