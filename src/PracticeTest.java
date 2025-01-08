ce633916016c6fab503d19ba4205f8d0800d9614
alstondsouza1 <alstondsouza35@gmail.com>
2025-01-08 11:34:30 -0800

    added test stubs for findEvens, subDiagonal, count Frequencies, and mostCommonSpaceefficient methods

--- src/PracticeTest.java
+++ src/PracticeTest.java
@@ -8,4 +8,27 @@ public class PracticeTest {
   
   // Hints: They are static methods, so you will use the full Practice.mostCommonTimeEfficient for method calls
   // Forgot how to do unit tests? Look back at the ramblebot tests or old slides for a refresher!
+
+  @Test
+  public void testFindEvens() {
+
+
+  }
+
+  @Test
+  public void testSumDiagonal() {
+
+
+  }
+
+  @Test
+  public void testCountFrequencies() {
+
+
+  }
+
+  @Test
+  public void testMostCommonSpaceEfficient() {
+    
+  }
 }
