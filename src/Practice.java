04f86db79a7faeaa49020e54cd182cbe98c9b1dc
mmaslov007 <maslov.maxwell@student.greenriver.edu>
2025-01-07 22:19:04 -0800

    Adjusted time complexity of mostCommonSpaceEfficient method

--- src/Practice.java
+++ src/Practice.java
@@ -87,7 +87,7 @@ public class Practice {
    * 
    * Once you finish, WRITE TESTS FOR IT in PracticeTest.java
    * 
-   * Time Complexity: O(n)
+   * Time Complexity: O(n^2)
    * Space Complexity: O(1)
    * 
    * @param nums An array of integers
