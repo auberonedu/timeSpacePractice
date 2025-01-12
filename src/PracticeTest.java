b307334ee3a89080d8b4e1328599fa3f54bfa172
Lizock <khyelyza@gmail.com>
2025-01-12 00:06:12 -0800

    Fixed the first test and added testMostCommonSpaceEfficient

--- src/PracticeTest.java
+++ src/PracticeTest.java
@@ -5,17 +5,22 @@ import static org.junit.jupiter.api.Assertions.*;
 public class PracticeTest {
   
   // TODO: Implement tests for Practice.mostCommonTimeEfficient and Practice.mostCommonSpaceEfficient
+  @Test
   public void testMostCommonTimeEfficient() {
     int[] numList1 = {1, 4, 4, 6, 7, 9, 3};
     int[] numList2 = {7, 7, 4, 2, 2, 1, 5};
 
     assertEquals(4, Practice.mostCommonTimeEfficient(numList1));
     assertEquals(2, Practice.mostCommonTimeEfficient(numList2));
+  }
 
-    // I need help fixing this. Is there a reson 
-    // why the assertEquals() is not working? Am 
-    // I setting this up wrong?
-
+  @Test
+  public void testMostCommonSpaceEfficient() {
+    int[] numList1 = {1, 4, 4, 6, 7, 9, 3};
+    int[] numList2 = {7, 7, 4, 2, 2, 1, 5};
+    
+    assertEquals(4, Practice.mostCommonSpaceEfficient(numList1));
+    assertEquals(7, Practice.mostCommonSpaceEfficient(numList2));
   }
 
   // Hints: They are static methods, so you will use the full Practice.mostCommonTimeEfficient for method calls
