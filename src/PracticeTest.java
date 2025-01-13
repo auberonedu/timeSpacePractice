29fd2679662a21bbc5a280fa5f1647da5aa74bf6
Tia Marie Gordon <tmgordon8192@gmail.com>
2025-01-13 13:21:17 -0800

    Created test case for problem 4

--- src/PracticeTest.java
+++ src/PracticeTest.java
@@ -8,4 +8,15 @@ public class PracticeTest {
 
   // Hints: They are static methods, so you will use the full Practice.mostCommonTimeEfficient for method calls
   // Forgot how to do unit tests? Look back at the ramblebot tests or old slides for a refresher!
+
+  @Test
+  void testMostCommonTimeEfficient() {
+    int[] nums = {1, 2, 2, 2, 3, 3, 3, 3, 4, 5, 5, 6, 7, 8, 9, 10};
+
+    Practice countTest = new Practice();
+
+    int actualCount = countTest.mostCommonTimeEfficient(nums);
+
+    assertEquals(3, actualCount);
+  }
 }
