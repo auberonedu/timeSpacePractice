c1b7450fec920bbfa80b9e098474c265cb301d7b
Kimberly Mageary <kimberlymageary@gmail.com>
2025-01-13 17:00:59 -0800

    Added some test cases

--- src/PracticeTest.java
+++ src/PracticeTest.java
@@ -4,11 +4,9 @@ import static org.junit.jupiter.api.Assertions.*;
 
 public class PracticeTest {
   
-  // TODO: Implement tests for Practice.mostCommonTimeEfficient and Practice.mostCommonSpaceEfficient
-  // Overall tests to see that the method is for the most part functioning
   @Test
   void testMostCommonTimeEffecient(){
-    //assertEquals(5, Practice.mostCommonTimeEfficient(new int[]{1, 3, 5, 2, 5, 5}));
+    assertEquals(5, Practice.mostCommonTimeEfficient(new int[]{1, 3, 5, 2, 5, 5}));
     assertEquals(8, Practice.mostCommonTimeEfficient(new int[]{8, 3, 4, 8, 5, 8}));
     assertEquals(1, Practice.mostCommonTimeEfficient(new int[]{1, 1, 1, 2, 5, 2}));
     assertEquals(2, Practice.mostCommonTimeEfficient(new int[]{1, 3, 4, 2, 5, 2}));
@@ -18,7 +16,12 @@ public class PracticeTest {
 
   @Test
   void testMostCommonSpaceEffecient(){
-
+    assertEquals(5, Practice.mostCommonTimeEfficient(new int[]{1, 3, 5, 2, 5, 5}));
+    assertEquals(8, Practice.mostCommonTimeEfficient(new int[]{8, 3, 4, 8, 5, 8}));
+    assertEquals(1, Practice.mostCommonTimeEfficient(new int[]{1, 1, 1, 2, 5, 2}));
+    assertEquals(2, Practice.mostCommonTimeEfficient(new int[]{1, 3, 4, 2, 5, 2}));
+    assertEquals(0, Practice.mostCommonTimeEfficient(new int[]{0,0,0}));
+    assertEquals(3, Practice.mostCommonTimeEfficient(new int[]{3}));
   }
 
 
